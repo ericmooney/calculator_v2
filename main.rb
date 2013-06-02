094782500abfac56d607272dfc2f0bd2884071b0
Eric Mooney <eric.c.mooney@gmail.com>
2013-06-02 14:04:58 -0700

    First Commit to remote

---
+++ main.rb
@@ -0,0 +1,26 @@
+require "sinatra"
+require "sinatra/reloader"
+
+get "/" do
+  erb :index
+end
+
+get "/calc" do
+  @first = params[:first].to_i
+  @second = params[:last].to_i
+
+  @operator = params[:operator]
+
+  case @operator
+  when "+"
+    @result = @first + @second
+  when "-"
+    @result = @first - @second
+  when "*"
+    @result = @first * @second
+  end
+
+  erb :calc       #sinatra knows to look to the calc.erb file
+end
+
+
